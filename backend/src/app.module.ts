94974a222b0fbbd5a8f9e130f6df035421aadf67
Helena <helenaff13@gmail.com>
2024-11-14 15:55:35 -0300

    feat: add dotenv for environment variable management and remove unused files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [/**/],
+      synchronize: true, // dev
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
